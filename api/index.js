ec3d38c6ad333d1f5009da86aa21248b67933f7e
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-04-23 01:22:17 +0530

    listing api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
-import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-
 dotenv.config();
 mongoose
   .connect("mongodb://localhost:27017/diya")
@@ -25,11 +23,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-app.use((err,req,res,next) =>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
-    success:false, statusCode,message,
+    success: false,
+    statusCode,
+    message,
   });
 });
